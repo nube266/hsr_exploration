cc1c2c72512c993611be9bb039bb34b226667d35
Lotfi El Hafi <lotfi.elhafi@gmail.com>
2019-04-07 16:12:57 +0900

    Pin core tool/package versions for improved stabiliy

--- INCL-SUDO-ENV.sh
+++ INCL-SUDO-ENV.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+################################################################################
+
+# Pin the versions of the core tools and packages for improved stabiliy.
 DOCKER_VERSION="5:18.09.3~3-0~ubuntu-$(lsb_release -cs)"
 DOCKER_COMPOSE_VERSION="1.23.2"
 NVIDIA_DOCKER_VERSION="2.0.3+docker18.09.3-1"
