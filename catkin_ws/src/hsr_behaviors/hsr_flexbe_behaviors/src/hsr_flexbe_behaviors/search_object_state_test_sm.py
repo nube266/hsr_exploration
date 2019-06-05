1d8d298224ad76bc05b2567972b1a2286872f7ba
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2019-06-05 14:12:20 +0900

    [add] Add search object state of FlexBE

---
+++ catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/search_object_state_test_sm.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from hsr_flexbe_states.hsr_set_base_pose_state import hsr_SetBasePoseState
+from hsr_flexbe_states.hsr_move_base_state import hsr_MoveBaseState
+from hsr_flexbe_states.hsr_search_object_state import hsr_SearchObjectState
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Wed Jun 05 2019
+@author: ShigemichiMatsuzaki
+'''
+class SearchObjectStateTestSM(Behavior):
+	'''
+	Test hsr_SearchObjectState
+	'''
+
+
+	def __init__(self):
+		super(SearchObjectStateTestSM, self).__init__()
+		self.name = 'Search Object State Test'
+
+		# parameters of this behavior
+		self.add_parameter('searching_point', 'searching_point_0')
+
+		# references to used behaviors
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:30 y:365, x:130 y:365
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:30 y:40
+			OperatableStateMachine.add('SetPoseSearchingPoint',
+										hsr_SetBasePoseState(pose_position_x=-1.0, pose_position_y=0.0, pose_orientation_z=3.14, pose_orientation_w=0),
+										transitions={'completed': 'MoveToSearchingPoint'},
+										autonomy={'completed': Autonomy.Off},
+										remapping={'pose': 'pose'})
+
+			# x:292 y:126
+			OperatableStateMachine.add('MoveToSearchingPoint',
+										hsr_MoveBaseState(),
+										transitions={'succeeded': 'SearchObject', 'failed': 'failed'},
+										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'request': 'pose'})
+
+			# x:30 y:209
+			OperatableStateMachine.add('SearchObject',
+										hsr_SearchObjectState(search_point=self.searching_point, search_place_type='floor', service_name='/search_object/search_floor'),
+										transitions={'succeeded': 'finished', 'failed': 'failed'},
+										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'pose': 'pose'})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
---
+++ catkin_ws/src/hsr_behaviors/hsr_flexbe_states/src/hsr_flexbe_states/hsr_search_object_state.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#
+# Active Intelligent Systems Laboratory
+# Toyohashi University of Technology
+# 
+# Shigemichi Matsuzaki
+#
+import rospy
+from flexbe_core import EventState
+from flexbe_core import Logger
+from flexbe_core.proxy import ProxyServiceCaller
+from search_object.srv import search_object_srv, search_object_srvRequest
+
+class hsr_SearchObjectState(EventState):
+    '''
+    A state to call search_object searvice used in Tidy Up task. 
+    Requred package : search_object (https://aisl-serv6.aisl.cs.tut.ac.jp:20443/wrs/search_object)
+
+    -- search_point          String    A tf name of a searching point
+    -- search_place_type     String    A type of the place to be checked {'floor'}
+
+    #> tf_name               String    The tf name of the location to go next
+    #> next_task             String    The task state name to be done next (FETCH, PUT, SEARCH etc.)
+
+    <= succeeded                       An object was found.
+    <= failed                          No object was found.
+    '''
+
+    def __init__(self, search_point, search_place_type, service_name):
+        super(hsr_SearchObjectState,self).__init__(outcomes=['succeeded', 'failed'],
+                                                    input_keys=['pose'])
+
+        self._search_point = search_point # The locations to be checked
+        self._search_place_type = search_place_type
+        self._service_name = service_name
+        self._search_object_server = ProxyServiceCaller({self._service_name : search_object_srv})
+
+    def execute(self, userdata):
+        '''
+        Execute the state
+        '''
+#        rospy.loginfo('Let\'s search')
+
+        # Wait for a second
+        rospy.sleep(1)
+        # If an object is found, return 'succeeded'
+        if not  self._failed:
+            rospy.loginfo('I found an object')
+            return 'succeeded'
+    
+        # If not found and there's another searching points, go to the next searching point and continue searching.
+        # If all the searching points have been checked, go to the initial location and end the task
+        else:
+            rospy.loginfo('I could\'nt find any objects')
+            return 'failed'
+
+
+    def on_enter(self, userdata):
+        pass
+        #
+        # Execute the searching motion
+        #
+        req = search_object_srvRequest('', self._search_place_type, self._search_point)
+        # res = self._search_object_server('', self._search_place_type, self._search_point)
+        self._failed = False
+        try:
+            self._srv_result = self._search_object_server.call(self._service_name, req)
+            rospy.loginfo(self._srv_result)
+        except Exception as e:
+            rospy.logwarn('Failed to call object recognizer:\n\r%s' % str(e))
+            self._failed = True
+    
+    def on_exit(self, userdata):
+        pass
+    
+    def on_start(self):
+        pass
+    
+    def on_stop(self):
+        pass
