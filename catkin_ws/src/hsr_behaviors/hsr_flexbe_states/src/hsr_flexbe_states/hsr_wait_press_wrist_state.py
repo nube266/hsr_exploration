55e55d0927b52483df3bcf1c44bfb6e05c05fadb
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2019-06-27 01:27:51 +0900

    [add] Add a new state 'hsr_wait_press_wrist_state'

---
+++ catkin_ws/src/hsr_behaviors/hsr_flexbe_states/src/hsr_flexbe_states/hsr_wait_press_wrist_state.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import rospy
+import actionlib
+from flexbe_core import EventState, Logger
+from hsrb_interface import Robot, ItemTypes
+
+class hsr_WaitWristPressedState(EventState):
+	'''
+           A state to wait for a user to press downwards the wrist.
+           By detecting the force on the joint, this state works as a start button.
+           This code simply copies the code described in https://qa.hsr.io/ja/question/1129/sutatobotanshi-jie-da-hui/ (Japanese)
+
+	<= succeeded				The force is detected = the wrist joint pressed
+	'''
+
+	def __init__(self):
+		super(hsr_WaitWristPressedState,self).__init__(outcomes=['succeeded'])
+                self._robot = Robot()
+
+	def execute(self, userdata):
+                return 'succeeded'
+
+
+	def on_enter(self, userdata):
+            while True:
+                wrench = self._robot.get('wrist_wrench',ItemTypes.FORCE_TORQUE).wrench
+                if wrench[0][0] > 20.0:
+                    break
+
+	def on_exit(self, userdata):
+		pass
+
+	def on_start(self):
+		pass
+
+	def on_stop(self):
+		pass
