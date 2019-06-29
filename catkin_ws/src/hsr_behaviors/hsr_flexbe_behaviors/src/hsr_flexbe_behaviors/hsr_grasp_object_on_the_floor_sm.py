246461b21d380c07235896c8e36dbad1898c92ca
mano <mano@aisl.cs.tut.ac.jp>
2019-06-29 10:50:30 +0900

    [add+update] Add new state 'hsr_MoveToNeutralState'

--- catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_grasp_object_on_the_floor_sm.py
+++ catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_grasp_object_on_the_floor_sm.py
@@ -8,9 +8,10 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from hsr_flexbe_states.hsr_search_object_state import hsr_SearchObjectState
+from hsr_flexbe_states.hsr_move_to_neutral_state import hsr_MoveToNeutralState
 from hsr_flexbe_states.hsr_fetch_object_state import hsr_FetchObjectState
 from hsr_flexbe_states.hsr_pass_object_state import hsr_PassObjectState
+from hsr_flexbe_states.hsr_search_object_state import hsr_SearchObjectState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -46,7 +47,7 @@ class HSRgraspobjectonthefloorSM(Behavior):
 
 
 	def create(self):
-		# x:30 y:365, x:130 y:365
+		# x:30 y:365, x:106 y:591
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 
 		# Additional creation code can be added inside the following tags
@@ -56,22 +57,34 @@ class HSRgraspobjectonthefloorSM(Behavior):
 
 
 		with _state_machine:
-			# x:30 y:40
-			OperatableStateMachine.add('SearchObject',
-										hsr_SearchObjectState(search_point=self.searching_point, search_place_type='floor', service_name='/search_object/search_floor', centroid_x_max=1.5, centroid_y_max=1.0, centroid_y_min=-1.0, centroid_z_max=0.1, centroid_z_min=0.0),
-										transitions={'succeeded': 'FetchObject', 'failed': 'failed'},
+			# x:45 y:51
+			OperatableStateMachine.add('MoveToNeutral',
+										hsr_MoveToNeutralState(),
+										transitions={'succeeded': 'SearchObject', 'failed': 'finished'},
 										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:292 y:141
+			# x:454 y:78
 			OperatableStateMachine.add('FetchObject',
 										hsr_FetchObjectState(fetch_place_type='floor', grasp_srv_name='/grasp/service', stop_tf_srv_name='/ork_tf_broadcaster/stop_publish', target_name='closest'),
-										transitions={'succeeded': 'Pass', 'failed': 'failed'},
+										transitions={'succeeded': 'Pass', 'failed': 'MoveToNeutralError'},
 										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:378 y:290
+			# x:289 y:461
+			OperatableStateMachine.add('MoveToNeutralError',
+										hsr_MoveToNeutralState(),
+										transitions={'succeeded': 'failed', 'failed': 'failed'},
+										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off})
+
+			# x:549 y:301
 			OperatableStateMachine.add('Pass',
 										hsr_PassObjectState(service_name='/kinesthetic/wait_open'),
-										transitions={'succeeded': 'finished', 'failed': 'failed'},
+										transitions={'succeeded': 'finished', 'failed': 'MoveToNeutralError'},
+										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off})
+
+			# x:237 y:53
+			OperatableStateMachine.add('SearchObject',
+										hsr_SearchObjectState(search_point=self.searching_point, search_place_type='floor', service_name='/search_object/search_floor', centroid_x_max=1.5, centroid_y_max=1.0, centroid_y_min=-1.0, centroid_z_max=0.3, centroid_z_min=0.0),
+										transitions={'succeeded': 'FetchObject', 'failed': 'MoveToNeutralError'},
 										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off})
 
 
