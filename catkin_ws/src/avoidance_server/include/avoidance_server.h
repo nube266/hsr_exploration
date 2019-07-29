ce23ba6d90a95b2ee9ff12e1e949a6e40b51d0d6
Shigemichimatsuzaki <Matsuzaki@aisl.cs.tut.ac.jp>
2019-07-30 03:38:03 +0900

    [add] Add avoidance server

---
+++ catkin_ws/src/avoidance_server/include/avoidance_server.h
@@ -0,0 +1,33 @@
+//
+// Active Intelligent Systems Laboratory
+// Toyohashi University of Technology
+//
+// Yusuke Miake
+//
+
+#ifndef AVOIDANCE_SERVER_H_
+#define AVOIDANCE_SERVER_H_
+
+#include "avoidance_server/avoidance_server.h"
+
+#include <ros/ros.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#include <string>
+
+namespace avoidance_server {
+class AvoidanceServer {
+  private:
+    ros::NodeHandlePtr nh_;  // Node handle
+    ros::Subscriber sub_;    // Subscriber for the clusters
+    ros::ServiceServer srv_; // Move to destination
+    // tf2_ros::Buffer tf_buffer_;
+
+  public:
+    AvoidanceServer(ros::NodeHandlePtr node_handle);
+    // void subscriberCallback(const visualization_msgs::MarkerArrayConstPtr& clusters_msg);
+    bool serviceCallback(avoidance_server::Request &req, avoidance_server::Response &res);
+};
+} // namespace avoidance_server
+
+#endif // AVOIDANCE_SERVER_H_
---
+++ catkin_ws/src/avoidance_server/src/avoidance_server.cpp
@@ -0,0 +1,40 @@
+//
+// Active Intelligent Systems Laboratory
+// Toyohashi University of Technology
+//
+// Yusuke Miake
+//
+
+#include "./../include/avoidance_server.h"
+
+namespace avoidance_server {
+
+AvoidanceServer::AvoidanceServer(ros::NodeHandlePtr node_handle) {
+    nh_ = node_handle;
+    // sub_ = nh_->subscribe<visualization_msgs::MarkerArray>("clusters", 1, &AvoidanceServer::subscriberCallback, this);
+    srv_ = nh_->advertiseService("/avoidance_server", &AvoidanceServer::serviceCallback, this);
+    ROS_INFO("Ready to avoidance_server");
+}
+
+/*
+void AvoidanceServer::subscriberCallback(const visualization_msgs::MarkerArrayConstPtr& clusters_msg) {
+    try{
+        // Listen ot a transform from base_link to camera frame to fix the X-Y plane parallel to the floor
+        transformStamped_ = tf_buffer_.lookupTransform("base_link", "head_rgbd_sensor_rgb_frame", ros::Time(0));
+        transformStampedInverse_ = tf_buffer_.lookupTransform("head_rgbd_sensor_rgb_frame", "base_link", ros::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+        ROS_WARN("%s",ex.what());
+        ros::Duration(1.0).sleep();
+        return;
+    }
+}
+*/
+
+bool AvoidanceServer::serviceCallback(avoidance_server::Request &req, avoidance_server::Response &res) {
+    ROS_INFO("Move Start");
+    res.is_succeeded = true;
+    return true;
+}
+
+} // namespace avoidance_server
