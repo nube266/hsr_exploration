16dabd89e8a7b5cb4fecf78195e1f708924760b4
Shigemichimatsuzaki <Matsuzaki@aisl.cs.tut.ac.jp>
2019-07-31 08:51:07 +0900

    WIP: Edit avoidance server

--- catkin_ws/src/avoidance_server/include/avoidance_server.h
+++ catkin_ws/src/avoidance_server/include/avoidance_server.h
@@ -8,20 +8,43 @@
 #ifndef AVOIDANCE_SERVER_H_
 #define AVOIDANCE_SERVER_H_
 
+// my header
 #include "avoidance_server/avoidance_server.h"
 
+// ROS
 #include <ros/ros.h>
-#include <string>
-#include <tf2_ros/transform_listener.h>
+#include <tf/transform_listener.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
+
+// PCL
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+// general
+#include <string>
+#include <vector>
+#include <tuple>
+
 
 namespace avoidance_server {
 class AvoidanceServer {
   private:
+    // ROS general
     ros::NodeHandlePtr nh_;  // Node handle
     ros::Subscriber sub_;    // Subscriber for the clusters
     ros::ServiceServer srv_; // Move to destination
+    // TF
     tf2_ros::Buffer tf_buffer_;
+    // geometry_msgs
+    geometry_msgs::TransformStamped transformStamped_;
+    geometry_msgs::TransformStamped transformStampedInverse_;
+
+    // point_cloud
+    std::tuple<int, char, std::string> t = std::make_tuple(1, 'a', "hello");
+    // using cluster_tuple = std::tuple<Eigen::Vector4f, Eigen::Matrix3f, std::string, int, pcl::PointXYZ, pcl::PointCloud<pcl::PointXYZ>::Ptr>;
+    // typedef std::tuple<Eigen::Vector4f, Eigen::Matrix3f, std::string, int, pcl::PointXYZ, pcl::PointCloud<pcl::PointXYZ>::Ptr> cluster_tuple;
 
   public:
     AvoidanceServer(ros::NodeHandlePtr node_handle);
--- catkin_ws/src/avoidance_server/src/avoidance_server.cpp
+++ catkin_ws/src/avoidance_server/src/avoidance_server.cpp
@@ -17,7 +17,6 @@ AvoidanceServer::AvoidanceServer(ros::NodeHandlePtr node_handle) {
 }
 
 void AvoidanceServer::subscriberCallback(const visualization_msgs::MarkerArrayConstPtr &clusters_msg) {
-    /*
     try{
         // Listen ot a transform from base_link to camera frame to fix the X-Y plane parallel to the floor
         transformStamped_ = tf_buffer_.lookupTransform("base_link", "head_rgbd_sensor_rgb_frame", ros::Time(0));
@@ -28,7 +27,49 @@ void AvoidanceServer::subscriberCallback(const visualization_msgs::MarkerArrayCo
         ros::Duration(1.0).sleep();
         return;
     }
-    */
+
+    tf::TransformListener listener;
+    // std::vector<cluster_tuple> cluster_tuples;
+    // for (auto cluster_it = clusters_msg->markers.begin(); cluster_it != clusters_msg->markers.end(); cluster_it++) {
+        /*
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cluster_pc(new pcl::PointCloud<pcl::PointXYZ>);
+        marker2PointCloud(*cluster_it, cluster_pc);
+
+        Eigen::Vector4f centroid;
+        Eigen::Vector3d centroid3d;
+        pcl::compute3DCentroid(*cluster_pc, centroid);
+
+        centroid3d << centroid[0], centroid[1], centroid[2];
+        // Transform
+        tf2::doTransform(centroid3d, centroid3d, transformStamped_);
+
+        centroid[0] = centroid3d[0];
+        centroid[1] = centroid3d[1];
+        centroid[2] = centroid3d[2];
+
+        // Check the thresholds
+        if(centroid(0) > centroid_x_max_ ||
+        (centroid(1) > centroid_y_max_ || centroid(1) < centroid_y_min_) || 
+        (centroid(2) > centroid_z_max_ || centroid(2) < centroid_z_min_)) {
+        ROS_WARN("Not within the threshold : %f %f %f", centroid(0), centroid(1), centroid(2));
+        ROS_WARN("               threshold : %f %f %f %f %f", centroid_x_max_, centroid_y_min_, centroid_y_max_, centroid_z_min_, centroid_z_max_);
+
+        continue;
+        }
+
+        pcl::PointXYZ near = getNearest(cluster_pc);
+        pcl::PCA<pcl::PointXYZ> pca = pcl::PCA<pcl::PointXYZ>(true);
+        pca.setInputCloud(cluster_pc);
+        // Vector in Camera frame
+        Eigen::Matrix3f basis_vector = pca.getEigenVectors();
+
+        // Make a tuple
+        cluster_tuple tpl(centroid, basis_vector, cluster_it->header.frame_id, cluster_it->id, near, cluster_pc);
+
+        // Store the tuple
+        cluster_tuples.push_back(tpl);
+        */
+    // }
 }
 
 bool AvoidanceServer::serviceCallback(avoidance_server::Request &req, avoidance_server::Response &res) {
