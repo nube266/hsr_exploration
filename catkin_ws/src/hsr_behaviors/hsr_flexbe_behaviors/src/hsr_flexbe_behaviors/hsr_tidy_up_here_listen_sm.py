8d7b4d4ee7a50e05d1c65bd325529c15c4dc85b7
Shigemichimatsuzaki <Matsuzaki@aisl.cs.tut.ac.jp>
2019-08-05 14:23:53 +0900

    [add] Add robot inspection behavior

--- catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_tidy_up_here_listen_sm.py
+++ catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_tidy_up_here_listen_sm.py
@@ -13,6 +13,8 @@ from flexbe_states.subscriber_state import SubscriberState
 from hsr_flexbe_states.hsr_speak_dyn_state import hsr_SpeakDynState
 from hsr_flexbe_states.hsr_analyse_command_state import hsr_AnalyseCommandState
 from flexbe_states.wait_state import WaitState
+from hsr_flexbe_states.hsr_set_base_pose_by_tf_name_state import hsr_SetBasePoseByTfNameState
+from hsr_flexbe_states.hsr_move_base_state import hsr_MoveBaseState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -59,49 +61,63 @@ class HSRTidyUpHereListenSM(Behavior):
 
 		with _state_machine:
 			# x:30 y:40
-			OperatableStateMachine.add('SpeakGiveMe',
-										hsr_SpeakState(sentence='How may I help you?', topic='/talk_request', interrupting=False, queueing=False, language=1),
-										transitions={'done': 'Wait1'},
-										autonomy={'done': Autonomy.Off})
+			OperatableStateMachine.add('SetPose',
+										hsr_SetBasePoseByTfNameState(tf_name='initial_point', service_name='/pose_server/getPose'),
+										transitions={'completed': 'Move'},
+										autonomy={'completed': Autonomy.Off},
+										remapping={'pose': 'pose'})
 
-			# x:243 y:114
+			# x:434 y:124
 			OperatableStateMachine.add('ListenObject',
 										SubscriberState(topic='/sr_res', blocking=True, clear=True),
 										transitions={'received': 'Analyse', 'unavailable': 'failed'},
 										autonomy={'received': Autonomy.Off, 'unavailable': Autonomy.Off},
 										remapping={'message': 'message'})
 
-			# x:209 y:217
+			# x:259 y:302
 			OperatableStateMachine.add('SpeakObject',
 										hsr_SpeakDynState(sentence="I'll bring +", sentence_when_empty="Could you say that again?", topic='/talk_request', interrupting=False, queueing=False, language=1),
 										transitions={'done': 'Wait2', 'empty': 'Wait1'},
 										autonomy={'done': Autonomy.Off, 'empty': Autonomy.Off},
 										remapping={'variable': 'object_name'})
 
-			# x:100 y:409
+			# x:485 y:471
 			OperatableStateMachine.add('ListenTidyUp',
 										SubscriberState(topic='/sr_res', blocking=True, clear=True),
 										transitions={'received': 'finished', 'unavailable': 'failed'},
 										autonomy={'received': Autonomy.Off, 'unavailable': Autonomy.Off},
 										remapping={'message': 'message'})
 
-			# x:441 y:102
+			# x:588 y:233
 			OperatableStateMachine.add('Analyse',
 										hsr_AnalyseCommandState(default_location='shelf', default_id='0', service_name='/wrs_semantics_bring/bring_me_instruction'),
 										transitions={'succeeded': 'SpeakObject', 'failed': 'failed'},
 										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'command': 'message', 'object_name': 'object_name', 'location_name': 'location_name', 'location_to_put': 'location_to_put'})
 
-			# x:119 y:102
+			# x:285 y:158
 			OperatableStateMachine.add('Wait1',
 										WaitState(wait_time=1.5),
 										transitions={'done': 'ListenObject'},
 										autonomy={'done': Autonomy.Off})
 
-			# x:122 y:285
+			# x:310 y:401
 			OperatableStateMachine.add('Wait2',
 										WaitState(wait_time=1.5),
 										transitions={'done': 'ListenTidyUp'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:230 y:41
+			OperatableStateMachine.add('Move',
+										hsr_MoveBaseState(),
+										transitions={'succeeded': 'SpeakGiveMe', 'failed': 'SpeakGiveMe'},
+										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'request': 'pose'})
+
+			# x:409 y:34
+			OperatableStateMachine.add('SpeakGiveMe',
+										hsr_SpeakState(sentence='How may I help you?', topic='/talk_request', interrupting=False, queueing=False, language=1),
+										transitions={'done': 'Wait1'},
 										autonomy={'done': Autonomy.Off})
 
 
--- catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_tidy_up_here_task_1_tidy_up_long_table_sm.py
+++ catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_tidy_up_here_task_1_tidy_up_long_table_sm.py
@@ -11,13 +11,14 @@ from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyC
 from hsr_flexbe_states.hsr_speak_state import hsr_SpeakState
 from hsr_flexbe_states.hsr_set_base_pose_by_tf_name_state import hsr_SetBasePoseByTfNameState
 from hsr_flexbe_states.hsr_move_base_state import hsr_MoveBaseState
-from hsr_flexbe_behaviors.hsr_fetchobject_sm import HSRFetchObjectSM
 from hsr_flexbe_states.hsr_fetch_object_interface_state import hsr_FetchObjectInterfaceState
 from hsr_flexbe_states.hsr_speak_dyn_state import hsr_SpeakDynState
 from hsr_flexbe_behaviors.hsr_move_sm import HSRMoveSM
 from hsr_flexbe_states.hsr_put_object_dyn_state import hsr_PutObjectDynState
 from hsr_flexbe_states.hsr_check_elapsed_time_state import hsr_CheckElapsedTimeState
 from hsr_flexbe_states.hsr_move_to_neutral_state import hsr_MoveToNeutralState
+from hsr_flexbe_behaviors.hsr_fetchobjectlongtable_sm import HSRFetchObjectLongTableSM
+from hsr_flexbe_states.hsr_escape_by_twist_state import hsr_EscapeByTwistState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -42,8 +43,8 @@ class HSRTidyUpHereTask1TidyUpLongTableSM(Behavior):
 		self.add_parameter('time_limit_task1', 15.0)
 
 		# references to used behaviors
-		self.add_behavior(HSRFetchObjectSM, 'HSR FetchObject')
 		self.add_behavior(HSRMoveSM, 'HSR Move_2')
+		self.add_behavior(HSRFetchObjectLongTableSM, 'HSR FetchObjectLongTable')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -86,17 +87,10 @@ class HSRTidyUpHereTask1TidyUpLongTableSM(Behavior):
 										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'request': 'pose'})
 
-			# x:360 y:163
-			OperatableStateMachine.add('HSR FetchObject',
-										self.use_behavior(HSRFetchObjectSM, 'HSR FetchObject'),
-										transitions={'finished': 'SpeakObjectName2', 'grasp_failed': 'SetPoseSearchingPointTallTable', 'not_found': 'finished', 'failed': 'failed'},
-										autonomy={'finished': Autonomy.Inherit, 'grasp_failed': Autonomy.Inherit, 'not_found': Autonomy.Inherit, 'failed': Autonomy.Inherit},
-										remapping={'search_centroid_y_max': 'search_centroid_y_max', 'search_centroid_y_min': 'search_centroid_y_min', 'search_centroid_z_max': 'search_centroid_z_max', 'search_centroid_z_min': 'search_centroid_z_min', 'search_sleep_time': 'search_sleep_time', 'search_is_floor': 'search_is_floor', 'search_centroid_x_max': 'search_centroid_x_max', 'object_name': 'object_name', 'location_name': 'location_name', 'location_to_put': 'location_to_put'})
-
 			# x:45 y:160
 			OperatableStateMachine.add('FetchObjectInterface2',
 										hsr_FetchObjectInterfaceState(centroid_x_max=1.0, centroid_y_max=0.6, centroid_y_min=-0.6, centroid_z_max=0.6, centroid_z_min=0.42, sleep_time=5.0, is_floor=False),
-										transitions={'done': 'HSR FetchObject'},
+										transitions={'done': 'HSR FetchObjectLongTable'},
 										autonomy={'done': Autonomy.Off},
 										remapping={'centroid_x_max': 'search_centroid_x_max', 'centroid_y_max': 'search_centroid_y_max', 'centroid_y_min': 'search_centroid_y_min', 'centroid_z_max': 'search_centroid_z_max', 'centroid_z_min': 'search_centroid_z_min', 'sleep_time': 'search_sleep_time', 'is_floor': 'search_is_floor'})
 
@@ -114,10 +108,10 @@ class HSRTidyUpHereTask1TidyUpLongTableSM(Behavior):
 										autonomy={'succeeded': Autonomy.Inherit, 'failed': Autonomy.Inherit},
 										remapping={'tf_name': 'location_name'})
 
-			# x:637 y:175
+			# x:798 y:321
 			OperatableStateMachine.add('Put2',
 										hsr_PutObjectDynState(put_place_type='shelf', service_name='/grasp/put'),
-										transitions={'succeeded': 'CheckElapsedTimeBeforeMoveToTable', 'failed': 'CheckElapsedTimeBeforePut'},
+										transitions={'succeeded': 'CheckElapsedTimeBeforeMoveToTable', 'failed': 'SendTwist'},
 										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'target_name': 'location_to_put'})
 
@@ -153,6 +147,19 @@ class HSRTidyUpHereTask1TidyUpLongTableSM(Behavior):
 										hsr_SpeakState(sentence='Time is up. Move on to the next task', topic='/talk_request', interrupting=False, queueing=False, language=1),
 										transitions={'done': 'MoveToNeutralTimeUp'},
 										autonomy={'done': Autonomy.Off})
+
+			# x:365 y:170
+			OperatableStateMachine.add('HSR FetchObjectLongTable',
+										self.use_behavior(HSRFetchObjectLongTableSM, 'HSR FetchObjectLongTable'),
+										transitions={'finished': 'SpeakObjectName2', 'grasp_failed': 'SetPoseSearchingPointTallTable', 'not_found': 'finished', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'grasp_failed': Autonomy.Inherit, 'not_found': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'search_centroid_y_max': 'search_centroid_y_max', 'search_centroid_y_min': 'search_centroid_y_min', 'search_centroid_z_max': 'search_centroid_z_max', 'search_centroid_z_min': 'search_centroid_z_min', 'search_sleep_time': 'search_sleep_time', 'search_is_floor': 'search_is_floor', 'search_centroid_x_max': 'search_centroid_x_max', 'object_name': 'object_name', 'location_name': 'location_name', 'location_to_put': 'location_to_put'})
+
+			# x:758 y:514
+			OperatableStateMachine.add('SendTwist',
+										hsr_EscapeByTwistState(topic='/hsrb/command_velocity', linear_x=0.0, linear_y=0.0, angular=0.3, duration=1.0),
+										transitions={'completed': 'HSR Move_2'},
+										autonomy={'completed': Autonomy.Off})
 
 
 		return _state_machine
