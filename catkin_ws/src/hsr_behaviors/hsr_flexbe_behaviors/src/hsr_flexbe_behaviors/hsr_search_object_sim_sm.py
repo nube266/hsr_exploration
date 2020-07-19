ad37968ba304693cd40512a3483c33b1a50c1c7d
YusukeMiake <miake@aisl.cs.tut.ac.jp>
2020-07-20 01:12:52 +0900

    Flex BEのbehaviorを修正

--- catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_search_object_sim_sm.py
+++ catkin_ws/src/hsr_behaviors/hsr_flexbe_behaviors/src/hsr_flexbe_behaviors/hsr_search_object_sim_sm.py
@@ -86,7 +86,7 @@ class HSRSearchObjectSimSM(Behavior):
 
 			# x:677 y:286
 			OperatableStateMachine.add('DetectObject',
-										hsr_ObjectDetectionState(target_object_1="apple", target_object_2="orange", target_object_3="", timeout=2, bounding_box_topic="/darknet_ros/bounding_boxes"),
+										hsr_ObjectDetectionState(target_object_1="keyboard", target_object_2="", target_object_3="", timeout=2, bounding_box_topic="/darknet_ros/bounding_boxes"),
 										transitions={'found': 'finished', 'not_found': 'EvaluateViewpoint'},
 										autonomy={'found': Autonomy.Off, 'not_found': Autonomy.Off})
 
