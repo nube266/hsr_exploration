e2d792bb8274d8f36f198dd31a59195562d63e28
YusukeMiake <miake@aisl.cs.tut.ac.jp>
2020-07-22 04:28:55 +0900

    tkinterのインストールを追加

---
+++ catkin_ws/src/viewpoint_planner_3d/scripts/viewpoint_planner_viewer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# h -*- coding: utf-8 -*-
+
+#
+# Active Intelligent Systems Laboratory
+# Toyohashi University of Technology
+#
+# Yusuke Miake
+#
+from Tkinter import *
+import rospy
+import time
+
+
+class ViewpointPlannerViewerNode:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    node = ViewpointPlannerViewerNode()
